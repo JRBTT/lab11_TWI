62d427064b0798f191a32c36ad51a1884721fd4f
JRBTT <jason.ray.buntting@gmail.com>
2024-05-17 07:29:03 +1000

    not finding slave

--- lib/twi/twi.cpp
+++ lib/twi/twi.cpp
@@ -6,7 +6,7 @@
 void twi_init_master(void) {
     // Set up TWI module for master mode
     TWSR = 0; // Set prescalar for 1
-    TWBR = 72; // Set bit rate register (see datasheet)
+    TWBR = 0x47; // Set bit rate register (see datasheet)
     TWCR = (1<<TWEN); // Enable TWI
 }
 
@@ -14,14 +14,30 @@ void twi_start(void) {
     // Send start condition
     TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);
     // Wait for TWINT Flag set. This indicates that the START condition has been transmitted
-    while (!(TWCR & (1<<TWINT)));
+    while ((TWCR & (1<<TWINT)) == 0);
 }
 
 void twi_stop(void) {
     // Transmit STOP condition
     TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);
+}
 
-    // Wait for stop condition to be executed on bus
-    // TWINT is not set after a stop condition!
-    while(TWCR & (1<<TWSTO));
+void twi_write(unsigned char data)
+{
+    // Load data into TWI data register
+    TWDR = data;
+    // Clear TWINT to start transmission
+    TWCR = (1 << TWINT) | (1 << TWEN);
+    // Wait for transmission to complete
+    while ((TWCR & (1 << TWINT)) == 0);
+}
+
+uint8_t twi_read_nack(void)
+{
+    // Start TWI read operation and do NOT acknowledge data after reception
+    TWCR = (1<<TWINT) | (1<<TWEN) | (0<<TWEA);
+    // Wait for TWINT flag set. This indicates that the DATA has been received
+    while((TWCR & (1<<TWINT)) == 0);
+    // Return received data
+    return TWDR;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -16,37 +16,24 @@ void twi_print_twsr()
   usart_tx_reg(TWSR);
   usart_transmit('\n');
 }
-
 int main(void)
 {
+  unsigned char data = 0;
   usart_init(103);
+  twi_print_twsr();
   twi_init_master();
-
-  // unsigned char data = 0;
-
-  // while(1)
-  // {
-  _delay_ms(1000);
   twi_print_twsr();
   twi_start();
   twi_print_twsr();
-  twi_stop();
+  twi_write(SLA_W);
   twi_print_twsr();
+  twi_write(data++);
+  twi_stop();
 
-
-    // twi_write(SLA_W);
-
-    // twi_write(data++)labs
-    // twi_stop();
-
-    // twi_start();
-
-    // twi_write(SLA_R);
-
-    // uint8_t received_data = twi_read_nack();
-
-    // twi_stop();
-
-    // usart_tx_float(received_data, 3, 0);
-  // }
+  twi_start();
+  twi_write(SLA_R);
+  uint8_t received_data = twi_read_nack();
+  twi_stop();
+  usart_tx_float(received_data, 3, 2);
+ 
 }
